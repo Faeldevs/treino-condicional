d3a78bcbc0c2f9ece15f4adf558a6710d045a2e3
Rafael S. Silva <103973828+Faeldevs@users.noreply.github.com>
2022-11-07 16:48:53 -0300

    Inserindo arquivo js

---
+++ 1-primeiroprojeto.js
@@ -0,0 +1,42 @@
+console.log(`\nCondicionais`);
+const listaDeDestinos = new Array(
+    `Chile`,
+    `Brasil`,
+    `Argentina`
+    `Bolivia`
+    `Peru`
+);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Colombia";
+
+console.log("\n Destinos possíveis:");
+console.log(listaDeDestinos);
+
+const podeComprar  = idadeComprador >= 18 || estaAcompanhada == true;
+
+let contador = 0;
+let destinoExiste = false;
+while(contador<3){
+    if(listaDeDestinos[contador] == destino){
+        destinoExiste = true;
+        break;
+    }
+    contador += 1;
+}
+
+console.log("Destino exite: ", destinoExiste);
+
+if(podeComprar && destinoExiste){
+    console.log("Boa Viagem");
+}else{
+    console.log("Desculpe tivemos um erro!");
+}
+
+for(let i  = 0 ; i < 3 ; i++){
+    if(listaDeDestinos[i] == destino){
+        destinoExiste = true;
+    }
+}
